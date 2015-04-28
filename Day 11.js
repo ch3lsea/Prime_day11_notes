abb06d5c3b4fa47d366e746f2b3b70cd73471ba6
Chelsea Okey <chelsea.okey@gmail.com>
2015-04-28 11:22:58 -0500

    first commit

---
+++ Day 11.js
@@ -0,0 +1,61 @@
+//Day 11
+
+//Picking up where we left off yesterday with the same bootstrap set up
+
+//Adding a picture at the top of a sub header col div--kitten
+
+//Adding well to the first sub head class section
+
+//color.adobe.com will help select colors
+//The explore tab at the top is super helpful for getting user generated color pallets that look nice
+
+//BOOTSTRAP SIZING of COLUMNS!!!
+//".col-xs-<size>" --from 0px to 77px (phone)
+//".col-sm-<size>" --from 78px to 991px (tablet)
+//".col-md-<size>" --from 992px to 1199px (Laptop)
+//".col-md-<size>" --1200+px (desktop)
+
+//class="hidden-sm" --this will hide the stuff inside this class when the size is small
+//<div class="hidden-sm hidden-xs">
+//will hide the div when the window size decreases to small and stays hidden when it decreases to xs
+
+//Lead class
+//text-left
+//text-right
+//text-center
+//text-justify
+//text-nowrap
+
+//Button sizes
+//btn-lg
+//btn-sm
+//btn-xs
+
+//Button Color
+//btn-default
+//btn-primary
+//btn-success
+//btn-info
+//btn-warning
+//btn-danger
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
